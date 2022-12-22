025396f166d5e1c1792617a3826a43fac9406f6f
MUKUND THAKUR <mukuthakur4008@gmail.com>
2022-12-22 21:30:35 +0530

    added select functionality

--- API/LearningRestAPI/controllers/products.js
+++ API/LearningRestAPI/controllers/products.js
@@ -1,7 +1,7 @@
 const Product=require("../models/product");
 
 const getMyproduct = async (req,res)=>{
-    const {company,name,featured,sort}=req.query;
+    const {company,name,featured,sort,select}=req.query;
     const queryobject={};
 
     if(company){
@@ -19,6 +19,12 @@ const getMyproduct = async (req,res)=>{
     if(sort){
         let sortFix=sort.replace(","," ");
         apiData=apiData.sort(sortFix);
+    }
+    if(select){
+        // let selectFix=select.replace(","," ");  //code brust when query for 3 param :name price,company can't able to remove from last
+        let selectFix=select.split(",").join(" ");
+        // console.log(selectFix);
+        apiData=apiData.select(selectFix);
     }
     const list=await apiData;
     res.status(200).json({list});
