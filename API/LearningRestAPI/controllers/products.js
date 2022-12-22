a71f43cde7566f087d52696d5653b8b4e9d54c96
MUKUND THAKUR <mukuthakur4008@gmail.com>
2022-12-22 16:05:29 +0530

    read data and show on web page

--- API/LearningRestAPI/controllers/products.js
+++ API/LearningRestAPI/controllers/products.js
@@ -1,5 +1,7 @@
+const Product=require("../models/product");
 const getMyproduct = async (req,res)=>{
-    res.status(200).json({msg : "fetch all products"});
+    const list=await Product.find({});
+    res.status(200).json({list});
 }
 const getMyproductToTest = async (req,res)=>{
     res.status(200).json({msg : "fetch all products for Testing."});
